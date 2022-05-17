fb9763388121beab62607e47c9665478fe679839
Lorel <lorel.oboue@gmail.com>
2022-05-17 14:11:15 +0100

    Data types-objects-objects and arrays challenges completed

--- src/data-types/objects/objects-and-arrays.js
+++ src/data-types/objects/objects-and-arrays.js
@@ -22,10 +22,22 @@ const basket = {
 // as well as modify some of the existing code
 
 // Set this variable to the length of the baskets voucher codes array
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length;
 
 // Set this variable to the first element in of the baskets voucher codes array
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0];
+
+//Price of apples updated
+basket.items[0].price = 2;
+
+
+//Oranges
+basket.items.push ({
+  name: 'Oranges',
+  quantity: 4,
+  price: 0.75
+} )
+
 
 // Do not edit this exported object
 module.exports = {
