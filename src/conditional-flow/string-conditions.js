e660ce35181ef3c15c974f87cf0b64d7e2cdebd0
Lorel <lorel.oboue@gmail.com>
2022-08-17 19:35:18 +0100

    some tests passed, two more to complete

--- src/conditional-flow/string-conditions.js
+++ src/conditional-flow/string-conditions.js
@@ -1,33 +1,33 @@
 // This function should return true if the passed string is equal to "Hello"
 
-function isHello (greeting) {
+const { e } = require('../loops/for-loop-and-arrays')
+
+function isHello(greeting) {
   // TODO: write code in this function body to pass the tests
   if (greeting === 'Hello') {
     return true
   } else {
-    return false 
+    return false
   }
 }
 
-
 // This function should return true if the passed string is not equal to "Hello"
 // const notHello = true;
-function isNotHello (val1) {
- // TODO: write code in this function body to pass the tests
- if(val1 !== 'Hello') {
-  return true
-} else {
-  return false
+function isNotHello(val1) {
+  // TODO: write code in this function body to pass the tests
+  if (val1 !== 'Hello') {
+    return true
+  } else {
+    return false
+  }
 }
-}
-
 
 // This function should return true if the string val1 is is longer
 // than string val2
-function isLongerThan (val1, val2) {
+function isLongerThan(val1, val2) {
   // TODO: write code in this function body to pass the tests
-  if(val1.length > val2.length) {
-    return true 
+  if (val1.length > val2.length) {
+    return true
   } else {
     return false
   }
@@ -36,20 +36,36 @@ isLongerThan('Mike', 'Ed')
 
 // This function should return true if the string passed in the function's first
 // argument has an odd number of vowels
+const vowels = ['a', 'e', 'i', 'o', 'u']
 
-function hasOddNumberVowels (val1) {
-
+function hasOddNumberVowels(val1) {
   // TODO: write code in this function body to pass the tests
-
+  // let count = 0
+  // for (let i = 0; i < val1.length; i++) {
+  //   if (val1.includes(vowels)) {
+  //     count++
+  //   }
+  // }
+  // // for (let i = val1.length; (i = 0); i++) {
+  // //   if (val1.includes(vowels)) {
+  // //     count++
+  // //   }
+  // // }
+  // if (count % 2 !== 0) {
+  //   return val1 === true
+  // } else {
+  //   return val1 === false
+  // }
 }
+
+// console.log('this is the count for mo', count)
 
 // this function should return the middle character of a string if it has an odd number
 // of characters. If there are an even number of characters the function should return
 // the middle two letters
 
-function getMiddleLetter (val1) {
+function getMiddleLetter(val1) {
   // TODO: write code in this function body to pass the tests
-
 }
 
 // This function should return the name of the season for the provided
@@ -61,9 +77,32 @@ function getMiddleLetter (val1) {
 // Summer - June to August
 // Autumn - September to November
 // Winter - December to February
-function seasonForMonth (monthName) {
-
+function seasonForMonth(monthName) {
   // TODO: write code in this function body to pass the tests
+
+  if (monthName === 'March' || monthName === 'April ' || monthName === 'May') {
+    return 'Spring'
+  } else if (
+    monthName === 'June' ||
+    monthName === 'July' ||
+    monthName === 'August'
+  ) {
+    return 'Summer'
+  } else if (
+    monthName === 'September' ||
+    monthName === 'October' ||
+    monthName === 'November'
+  ) {
+    return 'Autumn'
+  } else if (
+    monthName === 'December' ||
+    monthName === 'January' ||
+    monthName === 'February'
+  ) {
+    return 'Winter'
+  } else {
+    return ''
+  }
 }
 
 module.exports = {
