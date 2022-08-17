1e55ea418ede9c194d8b30bc4743ca7f5ee2e728
Lorel <lorel.oboue@gmail.com>
2022-08-17 14:19:58 +0100

    boolean conditions completed

--- src/conditional-flow/boolean-conditions.js
+++ src/conditional-flow/boolean-conditions.js
@@ -2,17 +2,14 @@
 // "Well done, you passed!" if the value is true, or "Sorry, try again"
 // if the value is false.
 
-
-function getResult (didPass) {
+function getResult(didPass) {
   // TODO: write code in this function body to pass the tests
   if (didPass) {
-    return "Well done, you passed!"
-     
-   } else {
-    return "Sorry, try again"
-   }
+    return 'Well done, you passed!'
+  } else {
+    return 'Sorry, try again'
+  }
 }
-
 
 module.exports = {
   a: getResult
